f6d1e1d5dfe0985cf58ab36c5cb81f2e0c0f30e1
Shandel <shandelvm16@gmail.com>
2025-04-02 11:26:01 -0300

    Database

---
+++ db_formpw.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 02/04/2025 às 14:24
+-- Versão do servidor: 8.3.0
+-- Versão do PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `db_formpw`
+--
+CREATE DATABASE IF NOT EXISTS `db_formpw` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `db_formpw`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tbl_usuarios`
+--
+
+DROP TABLE IF EXISTS `tbl_usuarios`;
+CREATE TABLE IF NOT EXISTS `tbl_usuarios` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `senha` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `tbl_usuarios`
+--
+
+INSERT INTO `tbl_usuarios` (`id_user`, `nome`, `email`, `senha`) VALUES
+(1, 'Shandel', 'shandelvm16@gmail.com', '$2y$10$NpI0FHui4rx2OzaCWVVYMeDt9gPjC3jZvgvzbJhJ0wvTlGSz6TpPK'),
+(2, 'Pug Master', 'pugmaster321@gmail.com', '$2y$10$QdFxGDlMp/usZWbkqHLNKeEJhwlPO4kJWsP9gri6IsEr8U.XzbIk2');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
